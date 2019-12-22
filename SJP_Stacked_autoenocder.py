5bfc313da60b611a785b97e983c2096b6c243e5d
sahil02235 <38981654+sahil02235@users.noreply.github.com>
2019-12-22 20:48:55 +0530

    Add files via upload

---
+++ SJP_Stacked_autoenocder.py
@@ -0,0 +1,119 @@
+# Stacked autoencoders
+# Importing the  libraries
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+# Importing the dataset
+# Import the movie dataset which contain the movie name, movie id and type of movie(ACtion, DRAma)
+movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+
+# user dataset , 1st column= user id; 2nd column =user gender; 3rd column = user age ; 4th column = user job id ; and the last colmn is the digit code 
+users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+
+# 1st column = user ; 2nd column = movies id; 3rd column = ratings ; 
+ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+
+# Preparing the training set and test set
+# Traning set has 80000 ratings
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+# Datatype (integer)
+training_set = np.array(training_set, dtype = 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype = 'int')
+
+# getting the number of users and movies (To create a Matrix of user, movie and ratings)
+
+nb_users = int(max(max(training_set[:,0]), max(test_set[:,0])))
+nb_movies = int(max(max(training_set[:,1]), max(test_set[:,1])))
+
+# Converting the data into an array with users in lines and movies in columns
+def convert(data):
+    new_data = []
+    for id_users in range(1, nb_users + 1):
+        id_movies = data[:,1][data[:,0] == id_users]
+        id_ratings = data[:,2][data[:,0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+training_set = convert(training_set)
+test_set = convert(test_set)
+
+# Converting the data into torch tensors
+training_set= torch.FloatTensor(training_set)
+test_set= torch.FloatTensor(test_set)
+
+#Architecture using Pytorch 
+class SAE(nn.Module):
+    def __init__(self, ):
+        super(SAE, self).__init__()
+        self.fc1 = nn.Linear(nb_movies, 20)
+        self.fc2 = nn.Linear(20, 10)
+        self.fc3 = nn.Linear(10, 20)
+        self.fc4 = nn.Linear(20, nb_movies)
+        self.activation = nn.Sigmoid()
+    
+    def forward(self, x):
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.activation(self.fc3(x))
+        x = self.fc4(x)
+        
+        return x
+
+# Create the Autoencoder
+sae = SAE()
+
+# Loss Function
+criterion = nn.MSELoss()
+
+# Optimizer
+optimizer = optim.RMSprop(sae.parameters(), lr = 0.01, weight_decay = 0.5)
+
+# Training the SAE
+nb_epoch = 200
+for epoch in range(1, nb_epoch + 1):
+    train_loss = 0
+    s = 0.
+    
+    for id_user in range(nb_users):
+        input = Variable(training_set[id_user]).unsqueeze(0)
+        target = input.clone()
+        if torch.sum(target.data > 0) >  0:
+            output = sae(input)
+            target.require_grad = False
+            output[target == 0] = 0
+            loss = criterion(output, target)
+            mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+            loss.backward()
+            train_loss += np.sqrt(loss.data[0]*mean_corrector)
+            s += 1.
+            optimizer.step()
+    print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
+
+# Testing the SAE
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    input = Variable(training_set[id_user]).unsqueeze(0)
+    target = Variable(test_set[id_user])
+    if torch.sum(target.data > 0) > 0:
+        output = sae(input)
+        target.require_grad = False
+        output[target == 0] = 0
+        loss = criterion(output, target)
+        mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+        test_loss += np.sqrt(loss.data[0]*mean_corrector)
+        s += 1.
+print('test loss: '+str(test_loss/s))
+
+    
+
+
